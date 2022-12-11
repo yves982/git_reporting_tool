ae09c3e2cc6bbf97be29c98f88f10d5601804855
Yves LALANNE <yves.lalanne@azeo.com>
2022-12-11 12:13:08 +0100

    fix test

--- tests/hexagon/models/test_feature_applier.py
+++ tests/hexagon/models/test_feature_applier.py
@@ -8,28 +8,30 @@ _rep: Repository | None = None
 
 
 @pytest.fixture(autouse=True)
-def setup(repository):
+def setup(mocker):
     global _feature_applier, _rep
-    _feature_applier = FeatureApplier(repository)
-    _rep = mocker(Repository)
+    _rep = mocker.patch("hexagon.use_cases.irepository.IRepository")
+    _feature_applier = FeatureApplier(_rep)
 
 
-def test_feature_applier_no_matching_commits():
+@pytest.mark.feature_applier
+def test_feature_applier_no_matching_commits(mocker):
     _rep.branches.return_value = ["PAIE-4322-test"]
     commit_spy = mocker.spy(_rep, "commits")
 
     res = _feature_applier.apply(r"PAIE-4429")
     assert commit_spy.call_count == 0, "commits should not be called"
-    assert res.status == ApplierStatus.NoMatch
+    assert res.status == ApplierStatus.No_match
 
 
+@pytest.mark.skip
 def test_feature_applier_single_matching_commit():
     pass
 
-
+@pytest.mark.skip
 def test_feature_applier_multiple_matching_commits():
     pass
 
-
+@pytest.mark.skip
 def test_feature_applier_repository_error():
     pass
